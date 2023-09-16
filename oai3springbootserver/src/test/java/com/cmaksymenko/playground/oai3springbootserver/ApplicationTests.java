0e5582fae4b254b238fb18bb90bd28aa42004569
Christian Maksymenko <christian.maksymenko@gmail.com>
2023-09-15 22:52:28 -0700

    Switched back to OAI 3.0.0 spec version and removed unsupported elements from Stoplight. Created blank OAI3 generated SpringBoot 3 project using the spec.

---
+++ oai3springbootserver/src/test/java/com/cmaksymenko/playground/oai3springbootserver/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmaksymenko.playground.oai3springbootserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
